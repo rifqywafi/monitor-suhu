dfbfc8317cd6c539391f762ac83e9e9bb1a3c29f
rifqywafi <rifqywafianerdza@gmail.com>
2025-12-18 00:36:33 +0700

    remove unused func

--- src/utils/SlidingWindow.js
+++
@@ -1,6 +0,0 @@
-export function updateSlidingWindow(prevData, newData, windowSize = 30) {
-  const merged = [...prevData, ...newData];
-
-  // ambil N data terakhir
-  return merged.slice(-windowSize);
-}
